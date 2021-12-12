46bec56a05275ec5b6c7d30b49e5c2f95182d5d9
John-Kodes <jdanielsemine@gmail.com>
2021-12-11 22:58:53 -0800

    header css complete

--- components/header.js
+++ components/header.js
@@ -1,9 +1,44 @@
+import Image from "next/image";
+// Image
+import portrait from "@/images/johndanielseminemacaranas.jpg";
+// Styles
 import styles from "@/styles/header.module.scss";
 
 const Header = () => {
+  const space = <>&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;</>;
+
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <div className={styles.content}>
+        <div className={styles.textBox}>
+          <h1 className={styles.name}>
+            John Daniel
+            <br />
+            <span>Semine Macaranas</span>
+          </h1>
+          <p className={styles.skills}>
+            Full-Stack Developer{space}MERN{space}Designer{space}Creative
+          </p>
+          <div className={styles.btnsContainer}>
+            <button className={`${styles.btn} ${styles.btnProject}`}>
+              Check some of my projects!
+            </button>
+            <button className={`${styles.btn} ${styles.btnAbout}`}>
+              About me
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.photo}>
+          <Image
+            src={portrait}
+            layout="fill"
+            alt="John Daniel Semine Macaranas"
+            objectFit="cover"
+            objectPosition="top"
+          />
+        </div>
+      </div>
     </header>
   );
 };
