00e94011f250d501d4fe2c7e05c3f821ef17ccc4
John-Kodes <jdanielsemine@gmail.com>
2021-12-13 20:36:51 -0800

    /ProjectsSect Project details added

--- components/ProjectsSect.js
+++ components/ProjectsSect.js
@@ -30,11 +30,11 @@ url('../public/polyBg.svg')`,
 
       <div className={styles.projectBox}>
         <div className={styles.textBox}>
-          <h3 className={styles.title}>Project Name</h3>
+          <h3 className={styles.title}>BlackJax (front-end)</h3>
           <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            laborum ipsum velit autem officia saepe atque vero voluptatem
-            quisquam. Molestias.
+            A recreation of the game “BlackJack” in the form of a web app
+            experience. Practice your skills, earn points and see how you place
+            on the leaderboard to see how you compare to other players.
           </p>
           <p className={styles.tags}>
             React{space}Redux{space}Router{space}Styled-Components
@@ -71,14 +71,14 @@ url('../public/polyBg.svg')`,
 
       <div className={styles.projectBox}>
         <div className={styles.textBox}>
-          <h3 className={styles.title}>Project Name</h3>
+          <h3 className={styles.title}>BlackJax API</h3>
           <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            laborum ipsum velit autem officia saepe atque vero voluptatem
-            quisquam. Molestias.
+            This RESTful API allows for users to create accounts which will be
+            stored using MongoDB. Sensitive user data is prevented from leaking
+            during API calls. bcrypt is used to encrypt user passwords.
           </p>
           <p className={styles.tags}>
-            React{space}Redux{space}Router{space}Styled-Components
+            Heroku{space}Node.js{space}Express.js{space}MongoDB{space}NoSQL
           </p>
           <div className={styles.btnsBox}>
             <Link href="/" passHref>
@@ -117,14 +117,14 @@ url('../public/polyBg.svg')`,
 
       <div className={styles.projectBox}>
         <div className={styles.textBox}>
-          <h3 className={styles.title}>Project Name</h3>
+          <h3 className={styles.title}>Internet Flicks</h3>
           <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            laborum ipsum velit autem officia saepe atque vero voluptatem
-            quisquam. Molestias.
+            An online database of information relating to movies, TV shows and
+            cast members. This project uses the TMDB API. The UI is inspired by
+            the streaming service, Netflix.
           </p>
           <p className={styles.tags}>
-            React{space}Redux{space}Router{space}Styled-Components
+            Next.js{space}TMDB API{space}Authentication
           </p>
           <div className={styles.btnsBox}>
             <Link href="/" passHref>
@@ -163,14 +163,15 @@ url('../public/polyBg.svg')`,
 
       <div className={styles.projectBox}>
         <div className={styles.textBox}>
-          <h3 className={styles.title}>Project Name</h3>
+          <h3 className={styles.title}>Restaurant Landing Page</h3>
           <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            laborum ipsum velit autem officia saepe atque vero voluptatem
-            quisquam. Molestias.
+            John Daniel&apos;s, the place to get the best burgers in Dubai (yes
+            this is based on a real restaurant). This is a mock up landing page
+            for a restaurant to answer the most common questions from current
+            and potentially new customers for high conversion rate.
           </p>
           <p className={styles.tags}>
-            React{space}Redux{space}Router{space}Styled-Components
+            React{space}Framer-motion{space}Styled-Components{space}Flexbox
           </p>
           <div className={styles.btnsBox}>
             <Link href="/" passHref>
