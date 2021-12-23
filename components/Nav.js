97e2022e9e5e2a01b65fdcc582a3b4e06c23db22
John-Kodes <jdanielsemine@gmail.com>
2021-12-22 18:52:19 -0800

    navPage for mobile and nav backdrop overlay done

--- components/Nav.js
+++ components/Nav.js
@@ -35,41 +35,69 @@ const Nav = ({ inView }) => {
     };
   }, [y]);
 
+  useEffect(() => {
+    if (!document) return;
+
+    if (!isActive) document.body.style.overflow = "hidden";
+    else document.body.style.overflowY = "scroll";
+  }, [isActive]);
+
   return (
-    <nav className={`${styles.nav} ${!scrollUp && !inView && styles.navHide}`}>
-      <div className={`${styles.logo} ${!inView && styles.logoHide}`}>
-        <Image
-          src={Logo}
-          alt="John Daniel Semine Macaranas Logo"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className={`${styles.btnsBox} ${inView && styles.btnsBoxInitial}`}>
-        <button
-          className={styles.link}
-          onClick={() => scrollToHandler("section-about")}
+    <>
+      <nav
+        className={`${styles.nav} ${!scrollUp && !inView && styles.navHide}`}
+      >
+        <div
+          className={`${styles.logo} ${
+            (!inView && styles.logoHide) || (!isActive && styles.logoFade)
+          }`}
         >
-          About
-        </button>
-        <button
-          className={styles.link}
-          onClick={() => scrollToHandler("section-projects")}
+          <Image
+            src={Logo}
+            alt="John Daniel Semine Macaranas Logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className={`${styles.btnsBox} ${inView && styles.btnsBoxInitial}`}>
+          <button
+            className={styles.link}
+            onClick={() => scrollToHandler("section-about")}
+          >
+            About
+          </button>
+          <button
+            className={styles.link}
+            onClick={() => scrollToHandler("section-projects")}
+          >
+            Projects
+          </button>
+          <button
+            className={styles.link}
+            onClick={() => scrollToHandler("section-contact")}
+          >
+            Contact
+          </button>
+          <button className={styles.resume}>Resume</button>
+        </div>
+        {/* MOBILE HAMBURGER NAV */}
+        <div
+          className={styles.hamburger}
+          onClick={() => setIsActive(!isActive)}
         >
-          Projects
-        </button>
-        <button
-          className={styles.link}
-          onClick={() => scrollToHandler("section-contact")}
-        >
-          Contact
-        </button>
-        <button className={styles.resume}>Resume</button>
-      </div>
-      <div className={styles.hamburger} onClick={() => setIsActive(!isActive)}>
-        <div className={`${styles.line} ${isActive && styles.lineActive}`} />
-      </div>
-    </nav>
+          <div className={`${styles.line} ${!isActive && styles.lineActive}`} />
+        </div>
+      </nav>
+      <ul className={`${styles.navPage} ${isActive && styles.navPageHide}`}>
+        <li className={styles.pageLink}>About</li>
+        <li className={styles.pageLink}>Projects</li>
+        <li className={styles.pageLink}>Contact</li>
+        <li className={styles.pageResume}>Resume</li>
+      </ul>
+      <div
+        className={`${styles.overlay} ${isActive && styles.overlayHidden}`}
+      />
+    </>
   );
 };
 
